d61ff878bcbd6988ac38bb3fc0be9021afe76c5b
André <andre@dev-next.com>
2020-07-08 23:22:00 -0300

    :sparkles: Added Textarea element

--- src/components/PForm/PForm.vue
+++ src/components/PForm/PForm.vue
@@ -5,15 +5,22 @@
       <p-input placeholder="Sabor" />
       <p-input placeholder="R$" type="number" />
     </div>
+
+    <div class="form__line">
+      <p-text-area />
+    </div>
   </div>
 </template>
 
 <script>
 import { PInput } from '@/components/PInput'
+import { PTextArea } from '@/components/PTextArea'
+
 export default {
   name: 'PForm',
   components: {
-    PInput
+    PInput,
+    PTextArea
   }
 }
 </script>
---
+++ src/components/PTextArea/PTextArea.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="text" :class="errorMessage && 'text--error'">
+    <textarea v-on="$listeners" v-bind="$attrs"></textarea>
+    <div v-if="errorMessage" class="text__message text__message--error">
+      {{ errorMessage }}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  inheritAttrs: false,
+  props: {
+    value: {
+      type: [String, Number]
+    },
+
+    errorMessage: {
+      type: String,
+      default: null
+    }
+  },
+
+  computed: {
+    listeners() {
+      return {
+        ...this.$listeners,
+        input: this.updateValue
+      }
+    }
+  },
+
+  methods: {
+    updateValue(e) {
+      this.$emit('input', e.target.value)
+    }
+  }
+}
+</script>
+
+<style></style>
