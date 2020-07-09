6d4fac4166c69a1b36965da7fc0c3c68bd2356be
André <andre@dev-next.com>
2020-07-08 23:06:31 -0300

    :sparkles: Added Input Component

--- src/components/PForm/PForm.vue
+++ src/components/PForm/PForm.vue
@@ -1,9 +1,21 @@
 <template>
-  <div></div>
+  <div class="form">
+    <div class="form__line">
+      <p-input placeholder="Titulo do Pedido" />
+      <p-input placeholder="Sabor" />
+      <p-input placeholder="R$" type="number" />
+    </div>
+  </div>
 </template>
 
 <script>
-export default {}
+import { PInput } from '@/components/PInput'
+export default {
+  name: 'PForm',
+  components: {
+    PInput
+  }
+}
 </script>
 
 <style></style>
---
+++ src/components/PInput/PInput.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="field">
+    <input v-on="$listeners" v-bind="$attrs" />
+    <div v-if="errorMessage" class="field__message field__message--error">
+      {{ errorMessage }}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  inheritAttrs: false,
+  props: {
+    value: {
+      type: [String, Number]
+    },
+
+    errorMessage: {
+      type: String,
+      default: null
+    },
+
+    mask: {
+      type: String,
+      default: null
+    }
+  },
+
+  methods: {
+    updateValue(e) {
+      this.$emit('input', e.target.value)
+    }
+  }
+}
+</script>
+
+<style></style>
