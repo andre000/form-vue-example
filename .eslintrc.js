070869740474e63d5ba531de7a6ade3399734dd7
André <andre@dev-next.com>
2020-07-08 21:37:44 -0300

    :rotating_light: Configuring eslint and prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,25 +1,30 @@
 module.exports = {
   root: true,
   env: {
-    node: true
+    es6: true,
+    node: true,
+    browser: true
   },
-  extends: ["plugin:vue/essential", "eslint:recommended", "@vue/prettier"],
+  extends: ['plugin:vue/essential', 'eslint:recommended', '@vue/prettier'],
   parserOptions: {
-    parser: "babel-eslint"
+    parser: 'babel-eslint'
   },
   rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off"
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-useless-escape': 0,
+    'no-empty': 0,
+    'comma-dangle': ['error', 'never'],
   },
   overrides: [
     {
       files: [
-        "**/__tests__/*.{j,t}s?(x)",
-        "**/tests/unit/**/*.spec.{j,t}s?(x)"
+        '**/__tests__/*.{j,t}s?(x)',
+        '**/tests/unit/**/*.spec.{j,t}s?(x)'
       ],
       env: {
         jest: true
       }
     }
   ]
-};
+}
