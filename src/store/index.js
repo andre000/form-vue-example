a2fe2003b20c04caa647c8de07acab8c763f4315
André Adriano <andre@dev-next.com>
2020-07-09 10:14:05 -0300

    :sparkles: Configured Vuex store

--- src/store/index.js
+++ src/store/index.js
@@ -4,8 +4,22 @@ import Vuex from 'vuex'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
-  modules: {}
+  state: {
+    orderType: 'food',
+    orders: []
+  },
+  mutations: {
+    CHANGE_ORDER_TYPE(state, type) {
+      state.orderType = type
+    },
+
+    ADD_ORDER(state, orders) {
+      state.orders.push({ ...orders, type: state.orderType })
+    }
+  },
+  getters: {
+    filteredOrders({ orders, orderType }) {
+      return orders.filter(o => o.type === orderType)
+    }
+  }
 })
