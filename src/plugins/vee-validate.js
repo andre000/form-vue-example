fcd7ee82ed8090c0fb029eb0d0512d8028691d57
André Adriano <andre@dev-next.com>
2020-07-10 10:38:39 -0300

    :sparkles: Added new rule and form validator component

--- src/plugins/vee-validate.js
+++ src/plugins/vee-validate.js
@@ -1,10 +1,19 @@
-import { ValidationProvider, extend, localize } from 'vee-validate'
-import { required, min, max } from 'vee-validate/dist/rules'
+import {
+  ValidationProvider,
+  ValidationObserver,
+  extend,
+  localize,
+  setInteractionMode
+} from 'vee-validate'
+import { required, min, max, min_value } from 'vee-validate/dist/rules'
 import ptBR from 'vee-validate/dist/locale/pt_BR.json'
 
 extend('required', required)
 extend('min', min)
 extend('max', max)
+extend('min_value', min_value)
+
+setInteractionMode('eager')
 
 localize({
   ptBR
@@ -12,4 +21,4 @@ localize({
 
 localize('ptBR')
 
-export default ValidationProvider
+export { ValidationProvider, ValidationObserver }
