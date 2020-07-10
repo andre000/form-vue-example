f342cd3b4fdff3493784a617718df35d937db7fc
André Adriano <andre@dev-next.com>
2020-07-10 10:35:52 -0300

    :truck: Renamed form component to OrderForm

--- src/components/PForm/index.js
+++
@@ -1,1 +0,0 @@
-export { default as PForm } from './PForm.vue'
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,6 @@
 <template>
   <section class="home">
-    <p-form />
+    <p-order-form />
     <p-order-list :orders="filteredOrders" />
   </section>
 </template>
@@ -8,13 +8,13 @@
 <script>
 import { mapGetters } from 'vuex'
 
-import { PForm } from '@/components/PForm'
+import { POrderForm } from '@/components/POrderForm'
 import { POrderList } from '@/components/POrderList'
 
 export default {
   name: 'Home',
   components: {
-    PForm,
+    POrderForm,
     POrderList
   },
 
