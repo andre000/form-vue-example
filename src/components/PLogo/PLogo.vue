231409e8048ce88a1005d63c673547351cd5d0a4
André <andre@dev-next.com>
2020-07-11 15:07:25 -0300

    :lipstick: Updated PLogo component to import SVG

--- src/components/PLogo/PLogo.vue
+++ src/components/PLogo/PLogo.vue
@@ -1,7 +1,43 @@
 <template>
   <figure>
-    <img src="@/assets/img/Logo.svg" alt="Logo" />
+    <logo />
   </figure>
 </template>
 
-<style></style>
+<script>
+import Logo from '@/assets/img/Logo.svg'
+
+export default {
+  components: {
+    Logo
+  }
+}
+</script>
+
+<style scoped lang="scss">
+.ae {
+  fill: #ea473c;
+}
+
+.a {
+  fill: #9e341d;
+}
+
+.b {
+  fill: #fbca28;
+}
+
+.bi {
+  fill: #5a5e69;
+}
+
+.ap {
+  fill: #565a64;
+}
+
+.l,
+.o,
+.n {
+  fill: #ecad28;
+}
+</style>
