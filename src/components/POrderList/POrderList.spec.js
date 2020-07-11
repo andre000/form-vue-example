2c4ed12701f7ad0d351290b35643f8275ba9a874
André <andre@dev-next.com>
2020-07-11 10:04:51 -0300

    :white_check_mark: Added POrderList test

---
+++ src/components/POrderList/POrderList.spec.js
@@ -0,0 +1,37 @@
+import { shallowMount, createLocalVue } from '@vue/test-utils'
+
+import POrderList from './POrderList'
+
+const localVue = createLocalVue()
+
+const mockedOrders = [
+  {
+    name: 'Suco de Goiaba',
+    flavour: 'Goiaba',
+    price: 5,
+    description: 'Um mero suco de goiaba. Tem sachet de açúcar no balcão...',
+    image: { preview: 'image' },
+    type: 'drinks'
+  }
+]
+
+describe('POrderList component', () => {
+  let wrapper
+  beforeAll(() => {
+    wrapper = shallowMount(POrderList, {
+      localVue
+    })
+  })
+
+  it('should match the snapshot', () =>
+    expect(wrapper.html()).toMatchSnapshot())
+
+  it('should have an ul element with .order-list class', () => {
+    expect(wrapper.find('ul.order-list').exists()).toBe(true)
+  })
+
+  it('should render "POrderList" component when order prop is received', async () => {
+    await wrapper.setProps({ orders: mockedOrders })
+    expect(wrapper.findComponent({ name: 'POrderItem' }).exists()).toBe(true)
+  })
+})
