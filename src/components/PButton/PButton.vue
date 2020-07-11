2b303865928acff06f04c74d9c64e1344ab0e056
André <andre@dev-next.com>
2020-07-11 20:34:50 -0300

    :lipstick: Styles PButton component

--- src/components/PButton/PButton.vue
+++ src/components/PButton/PButton.vue
@@ -17,4 +17,40 @@ export default {
 }
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+@import '@/assets/scss/variables.scss';
+
+.button {
+  width: 200px;
+  height: 60px;
+  border-radius: 30px;
+  outline: none;
+  border: none;
+  font: 900 20px/26px Roboto;
+  text-transform: uppercase;
+  cursor: pointer;
+  transition: filter 0.3s ease;
+
+  &:hover {
+    filter: drop-shadow(2px 4px 6px $shadow-color);
+  }
+  &:disabled {
+    filter: contrast(0.5) grayscale(0.6);
+    cursor: not-allowed;
+  }
+
+  &.button--default {
+    background: #eee;
+  }
+
+  &.button--primary {
+    background: $primary;
+    color: #fff;
+  }
+
+  &.button--secondary {
+    background: $secondary;
+    color: $primary-text;
+  }
+}
+</style>
