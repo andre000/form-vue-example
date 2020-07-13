7520507e1cc75f9f6153c925ecb8ba770b33631c
André Adriano <andre@dev-next.com>
2020-07-13 09:48:06 -0300

    :white_check_mark: Add knobs to PButton storybook

--- src/components/PButton/PButton.stories.js
+++ src/components/PButton/PButton.stories.js
@@ -1,26 +1,44 @@
 import { action } from '@storybook/addon-actions'
+import { withKnobs, text, boolean } from '@storybook/addon-knobs'
 import PButton from './PButton.vue'
 
 export default {
   component: PButton,
-  title: 'PButton'
+  title: 'PButton',
+  decorator: [withKnobs]
 }
 
 export const withText = () => ({
   components: { PButton },
-  template: '<p-button @click="action">Hello Button</p-button>',
+  props: {
+    text: {
+      default: text('Text', 'Default Button')
+    }
+  },
+  template: '<p-button @click="action">{{ text }}</p-button>',
   methods: { action: action('clicked') }
 })
 
 export const withPrimaryColor = () => ({
   components: { PButton },
-  template: '<p-button @click="action" color="primary">Hello Button</p-button>',
+  props: {
+    disabled: {
+      default: boolean('Disabled', false)
+    }
+  },
+  template:
+    '<p-button @click="action" :disabled="disabled" color="primary">Button</p-button>',
   methods: { action: action('clicked') }
 })
 
 export const withSecondaryColor = () => ({
   components: { PButton },
+  props: {
+    disabled: {
+      default: boolean('Disabled', false)
+    }
+  },
   template:
-    '<p-button @click="action" color="secondary">Hello Button</p-button>',
+    '<p-button @click="action" :disabled="disabled" color="secondary">Button</p-button>',
   methods: { action: action('clicked') }
 })
